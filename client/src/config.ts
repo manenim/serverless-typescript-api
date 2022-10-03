5d268fd9008992d6f074b47ee7050257fa45132f
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2022-10-03 16:45:11 +0100

    completed the application and all functionalities work perfectly

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'bf1x5kn80d';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-fdxj0um9.us.auth0.com',
+  clientId: 'iaWgJi4BCvKqNapcI4ZKA8TwxGKs6y2u',
+  callbackUrl: 'http://localhost:3000/callback'
+};
